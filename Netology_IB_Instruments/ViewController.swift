f0887ad988dda7bef7886c488327acefe4ea369a
Denisov Alexey <denisovalexey88@mail.ru>
2022-05-13 19:15:28 +0500

    Initial Commit

---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Алексей Денисов on 13.05.2022.
+//  Copyright © 2022 Алексей Денисов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
