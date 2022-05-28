3c3d589d581781c4cd145ede492d7614e707b11a
Denisov Alexey <denisovalexey88@mail.ru>
2022-05-28 15:55:43 +0300

    Задание 2.5

---
+++ Profile/PhotosCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewCell.swift
+//  Navigation
+//
+//  Created by Алексей Денисов on 25.05.2022.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Profile/PhotosViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotosViewController.swift
+//  Navigation
+//
+//  Created by Алексей Денисов on 25.05.2022.
+//
+
+import UIKit
+
+class PhotosViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
